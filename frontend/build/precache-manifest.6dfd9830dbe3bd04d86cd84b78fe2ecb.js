4e2290970eab5b80d9853a958a50e65e263c8aeb
CodeDecoder01 <47826916+CodeDecoder01@users.noreply.github.com>
2020-09-21 08:28:05 +0530

    BAEON 1.0.0 build

---
+++ frontend/build/precache-manifest.6dfd9830dbe3bd04d86cd84b78fe2ecb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b65da58bed5cd6f3abbc2c4d125a009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f48e4b458678c7b626d",
+    "url": "/static/css/main.5b833269.chunk.css"
+  },
+  {
+    "revision": "e50ecd372fe638e1ea84",
+    "url": "/static/js/2.f15a0d91.chunk.js"
+  },
+  {
+    "revision": "d454c0b05b1d881a2947f3585183a7ea",
+    "url": "/static/js/2.f15a0d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f48e4b458678c7b626d",
+    "url": "/static/js/main.9bd7b6a8.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "9845d0c3abbdf2adadd9355e47edd167",
+    "url": "/static/media/Ico.9845d0c3.jpg"
+  },
+  {
+    "revision": "6d8d27d2875c7103e97859dddff7f4b9",
+    "url": "/static/media/LogoBgImage.6d8d27d2.png"
+  },
+  {
+    "revision": "b68a9c2428b0b398f48ffb24ef080c25",
+    "url": "/static/media/apiBg.b68a9c24.jpg"
+  },
+  {
+    "revision": "d757f5fcfb8e770b5e59047a2f134f30",
+    "url": "/static/media/bg2.d757f5fc.jpg"
+  },
+  {
+    "revision": "62d35051c7a598a2b3659345d7a2a0e4",
+    "url": "/static/media/gifts.62d35051.jpg"
+  },
+  {
+    "revision": "162c65a57a1ab2dfffd8f76fec50c5a8",
+    "url": "/static/media/growth.162c65a5.png"
+  },
+  {
+    "revision": "1550512455137924e2f5b647412c3838",
+    "url": "/static/media/market.15505124.jpg"
+  },
+  {
+    "revision": "0da55187e79027825492d008e39b68ee",
+    "url": "/static/media/reach.0da55187.png"
+  },
+  {
+    "revision": "323e4b194f9093974e2b9c9b38fc8f38",
+    "url": "/static/media/reset.323e4b19.JPG"
+  },
+  {
+    "revision": "dae3e1673be6917a79bd78bbbc5f23f0",
+    "url": "/static/media/sales2.dae3e167.jpg"
+  }
+]);
